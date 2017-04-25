42109860dcc149cda7b1d9ae171d049ec5b83379
Julien Godfroid <kragg77@hotmail.com>
2017-04-25 13:05:15 -0400

    1.0.3
    Yet another take at perf issues

--- endApp.py
+++ endApp.py
@@ -7,19 +7,18 @@ class ApplicationExtension(ApplicationLevelExtension):
     def end_application(self, application):
         logging.info('Creating links to iBatis objects...')
         
-        for classObject in application.objects().has_type([ 'CAST_DotNet_ConstructorCSharp',
-                                                            'CAST_DotNet_ConstructorExternal',
-                                                            'CAST_DotNet_ConstructorVB',
-                                                            'JV_CTOR',
-                                                            'JV_GENERIC_CTOR',
-                                                            'JV_INST_CTOR']):
-            #logging.debug('WTF %s.%s' % (namedQuery.get_property(prop), classObject.get_name()))
-            iBatisLinks.create(application, classObject)
-        
-        for methodObject in application.search_objects(category='JV_METHOD', load_properties=True):
-            MyBatisLinks.create(application, methodObject)                        
+        for namedQuery in application.search_objects(category='CAST_SQL_NamedQuery', load_properties=True):
+            logging.debug('Processing Name query %s' % namedQuery.get_name())
+            
+            iBatisLinks.create(application, namedQuery, 'iBatisProperties.class')
+            iBatisLinks.create(application, namedQuery, 'iBatisProperties.parameterClass')
+            iBatisLinks.create(application, namedQuery, 'iBatisProperties.resultClass')
+            iBatisLinks.create(application, namedQuery, 'iBatisProperties.listClass')
+            iBatisLinks.create(application, namedQuery, 'iBatisProperties.resultType')
+            MyBatisLinks.create(application, namedQuery)                    
         
         #GREP in DotNet/Java, looking for iBatis objects
+        logging.info('Creating dynamic links to iBatis objects...')
         rf = ReferenceFinder()
         rf.add_pattern('iBatis', before='["\.]', element='[A-Za-z0-9_\-]+', after='"')
         
@@ -35,29 +34,45 @@ class ApplicationExtension(ApplicationLevelExtension):
             for namedQuery in application.objects().has_type('CAST_SQL_NamedQuery'):
                 if reference.value == namedQuery.get_name():
                     logging.debug('Reference to %s found in %s' % (namedQuery.get_name(), reference.object.get_name()))
-                    create_link('callLink', reference.object, namedQuery, reference.bookmark)
+                    link = create_link('callLink', reference.object, namedQuery, reference.bookmark)
+                    link.mark_as_not_sure()
 
 class MyBatisLinks():                
     @staticmethod
-    def create(application, methodObject):
-        for namedQuery in application.search_objects(category='CAST_SQL_NamedQuery', load_properties=True):
-            #logging.debug('MYBATIS %s == %s.%s' % (methodObject.get_fullname(), namedQuery.get_property('iBatisProperties.namespace'), namedQuery.get_name()));
+    def create(application, namedQuery):   
+        logging.debug('Looking for %s' % namedQuery.get_name())     
+        for methodObject in application.search_objects(name=namedQuery.get_name(), category='JV_METHOD'):
             if methodObject.get_fullname() == '%s.%s' % (namedQuery.get_property('iBatisProperties.namespace'), namedQuery.get_name()):
                 logging.debug('MyBatis structural link from %s to %s' % (methodObject.get_fullname(), namedQuery.get_name()))
                 create_link('useLink', methodObject, namedQuery)
-    
+
 class iBatisLinks():
     @staticmethod
-    def create(application, classObject):
-        for namedQuery in application.search_objects(category='CAST_SQL_NamedQuery', load_properties=True):
-            iBatisLinks.createForProp(application, classObject, namedQuery, 'iBatisProperties.class')
-            iBatisLinks.createForProp(application, classObject, namedQuery, 'iBatisProperties.parameterClass')
-            iBatisLinks.createForProp(application, classObject, namedQuery, 'iBatisProperties.resultClass')
-            iBatisLinks.createForProp(application, classObject, namedQuery, 'iBatisProperties.listClass')
-            iBatisLinks.createForProp(application, classObject, namedQuery, 'iBatisProperties.resultType')
-            
+    def create(application, namedQuery, prop):
+        if namedQuery.get_property(prop) is None:
+            return
+        
+        if namedQuery.get_property(prop).find('.') == -1:
+            classname = namedQuery.get_property(prop)
+        else:
+            classname = namedQuery.get_property(prop).split('.')[-1]
+        
+        logging.debug('Looking for %s' % classname)
+        for constructorObject in application.search_objects(name=classname, category='CAST_DotNet_ConstructorCSharp'):
+            iBatisLinks.create_iBatisLink(application, namedQuery, prop, classname, constructorObject)
+        for constructorObject in application.search_objects(name=classname, category='CAST_DotNet_ConstructorExternal'):
+            iBatisLinks.create_iBatisLink(application, namedQuery, prop, classname, constructorObject) 
+        for constructorObject in application.search_objects(name=classname, category='CAST_DotNet_ConstructorVB'):
+            iBatisLinks.create_iBatisLink(application, namedQuery, prop, classname, constructorObject) 
+        for constructorObject in application.search_objects(name=classname, category='JV_CTOR'):
+            iBatisLinks.create_iBatisLink(application, namedQuery, prop, classname, constructorObject) 
+        for constructorObject in application.search_objects(name=classname, category='JV_GENERIC_CTOR'):
+            iBatisLinks.create_iBatisLink(application, namedQuery, prop, classname, constructorObject) 
+        for constructorObject in application.search_objects(name=classname, category='JV_INST_CTOR'):
+            iBatisLinks.create_iBatisLink(application, namedQuery, prop, classname, constructorObject)      
+    
     @staticmethod
-    def createForProp(application, classObject, namedQuery, prop):
-        if classObject.get_fullname() == '%s.%s' % (namedQuery.get_property(prop), classObject.get_name()):
-            logging.debug('iBatis structural link from %s to %s' % (namedQuery.get_name(), classObject.get_fullname()))
-            create_link('referLink', namedQuery, classObject)
+    def create_iBatisLink(application, namedQuery, prop, classname, constructorObject):  
+        if constructorObject.get_fullname() == '%s.%s' % (namedQuery.get_property(prop), classname):
+            logging.debug('iBatis structural link from %s to %s' % (namedQuery.get_name(), constructorObject.get_fullname()))
+            create_link('referLink', namedQuery, constructorObject)
